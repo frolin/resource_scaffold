c93b1efcd12fa88fc91c43e6b913de4a9f81ec24
Andre Frolov <aafrolov@ceki.ru>
2021-04-25 19:25:05 +0300

    add bootstrap views

--- lib/generators/resource_scaffold/controller_generator.rb
+++ lib/generators/resource_scaffold/controller_generator.rb
@@ -8,18 +8,17 @@ module ResourceScaffold
     # Custom scaffolding generator
     class ControllerGenerator < Rails::Generators::NamedBase
       include Rails::Generators::ResourceHelpers
-      # include ResourceScaffold::Generators::GeneratorHelpers
+
+      include ResourceScaffold::Generators::GeneratorHelpers
       check_class_collision suffix: "Controller"
 
       class_option :helper, type: :boolean
       class_option :orm, banner: "NAME", type: :string, required: true,
-                         desc: "ORM to generate the controller for"
+                   desc: "ORM to generate the controller for"
       class_option :api, type: :boolean,
-                         desc: "Generates API controller"
+                   desc: "Generates API controller"
 
       class_option :skip_routes, type: :boolean, desc: "Don't add routes to config/routes.rb."
-
-      argument :attributes, type: :array, default: [], banner: "field:type field:type"
 
       source_root File.expand_path('../templates', __FILE__)
 
@@ -27,6 +26,15 @@ module ResourceScaffold
 
       def copy_controller_and_spec_files
         template "controller.rb", File.join("app/controllers", "#{controller_file_name}_controller.rb")
+      end
+
+      def copy_view_files
+        directory_path = File.join("app/views", controller_file_path)
+        empty_directory directory_path
+
+        view_files.each do |file_name|
+          template "views/#{file_name}.html.erb", File.join(directory_path, "#{file_name}.html.erb")
+        end
       end
 
     end
--- lib/generators/resource_scaffold/generator_helpers.rb
+++ lib/generators/resource_scaffold/generator_helpers.rb
@@ -1,7 +1,40 @@
 module ResourceScaffold
   module Generators
     # Some helpers for generating scaffolding
-    module ResourceHelpers # :nodoc:
+    module GeneratorHelpers
+      attr_accessor :options, :attributes
+
+      private
+
+      def view_files
+        %w(index new edit _form show)
+      end
+
+      def model_columns_for_attributes
+        class_name.constantize.columns.reject do |column|
+          column.name.to_s =~ /^(id|user_id|created_at|updated_at)$/
+        end
+      end
+
+      def editable_attributes
+        attributes ||= model_columns_for_attributes.map do |column|
+          Rails::Generators::GeneratedAttribute.new(column.name.to_s, column.type.to_s)
+        end
+      end
+
+      def controller_methods(dir_name)
+        all_actions.map do |action|
+          read_template("#{dir_name}/#{action}.rb")
+        end.join("n").strip
+      end
+
+      def read_template(relative_path)
+        ERB.new(File.read(source_path(relative_path)), nil, '-').result(binding)
+      end
+
+      def source_path(relative_path)
+        File.expand_path(File.join("../templates/", relative_path), __FILE__)
+      end
 
     end
   end
